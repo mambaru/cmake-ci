fab848250047a7ced1be1e991d9d3191397de09a
Vladimir Migashko <migashko@gmail.com>
2019-02-28 00:54:54 +0300

    repos.cmake fix

--- cmake/repos.cmake
+++ cmake/repos.cmake
@@ -1,7 +1,2 @@
-set(faslib_REPO "git@github.lan:cpp/faslib.git")
-set(REPO_PREFIX "git@github.lan:cpp")
-
-#Кастомный вариант для либы, например faslib
-#set(faslib_REPO "https://github.com/migashko/faslib.git")
-#Для всех остальных библиотек, если они лежат в одном месте 
-#set(REPO_PREFIX "https://github.com/mambaru")
+set(faslib_REPO "https://github.com/migashko/faslib.git")
+set(REPO_PREFIX "https://github.com/mambaru")
