e8a59d6929c1321330581a474a650459088ae466
Vladimir Migashko <migashko@gmail.com>
2019-03-17 21:34:56 +0300

    *cmake

--- cmake/opt.cmake
+++ cmake/opt.cmake
@@ -8,19 +8,33 @@ endif()
 
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
 option(BUILD_SHARED_LIBS "Build the shared library" ON)
+option(CODE_COVERAGE "Build with code coverage" OFF)
+option(BUILD_TESTING "Build with tests and samples" OFF)
+option(WITH_SAMPLES "Build with samples" ON)
+option(PARANOID_WARNING "Paranoid warning level" OFF)
+
 if (NOT CMAKE_BUILD_TYPE)
   message(STATUS "No build type selected, default to Release")
   set(CMAKE_BUILD_TYPE "Release")
 endif()
 
+if ( CODE_COVERAGE )
+  set(BUILD_TESTING ON)
+  set(WITH_SAMPLES OFF)
+  set(CMAKE_BUILD_TYPE "Debug")
+  set(CMAKE_CXX_FLAGS_DEBUG "-fprofile-arcs -ftest-coverage")
+endif()
+
 if ( ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang") 
       OR ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU") )
       
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -W -Wall -Werror -pedantic -ftemplate-depth=1024 -ftemplate-backtrace-limit=0 -fpic")
-  set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG ")
-  set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "-O2 -g -DNDEBUG")
-  set(CMAKE_CXX_FLAGS_DEBUG  "-O0 -g")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -W -Wall -Werror -pedantic -ftemplate-depth=1024 -ftemplate-backtrace-limit=0")
+  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3 -DNDEBUG ")
+  set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -O2 -g -DNDEBUG")
+  set(CMAKE_CXX_FLAGS_DEBUG  "${CMAKE_CXX_FLAGS_DEBUG} -O0 -g")
   
   if ( "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" )
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__STRICT_ANSI__ ${CLANG_LIBPP} ")   #-stdlib=libc++
@@ -29,6 +43,7 @@ if ( ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
   if ( PARANOID_WARNING )
   
     if ( CMAKE_COMPILER_IS_GNUCXX )
+    
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wlogical-op  -Wnoexcept -Wstrict-null-sentinel ")
       
       if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 4.9)
@@ -43,10 +58,11 @@ if ( ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
     if ( "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" )
     endif()
   
+    # -Wlong-long ?? 
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra -ftemplate-depth=1400 -Wcast-align -Winvalid-pch -pedantic-errors  -Wformat-nonliteral")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wcomment -Wconversion -Wformat-security -Wimport  -Wchar-subscripts")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat-y2k")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wlong-long -Wmissing-braces -Wmissing-field-initializers")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}  -Wmissing-braces -Wmissing-field-initializers")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wmissing-format-attribute -Wpacked -Wparentheses -Wpointer-arith")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wreturn-type -Wsequence-point -Wsign-compare -Wuninitialized")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wstrict-aliasing -Wstrict-aliasing=2 -Wswitch-enum -Wtrigraphs -Wunknown-pragmas")
@@ -64,6 +80,18 @@ if ( ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
 # using Intel C++
 endif()
+
+if ( CMAKE_CXX_STANDARD EQUAL 98 )
+  option(DISABLE_LONG_LONG_98 "Disable long-long for 98" OFF)
+  if ( DISABLE_LONG_LONG_98 )
+    add_definitions( -DDISABLE_LONG_LONG_98=OFF )
+  else()
+    add_definitions( -DDISABLE_LONG_LONG_98=OFF )
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-long-long")
+  endif()
+  
+endif()
+
 
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/archive")
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE}/bin/lib")
