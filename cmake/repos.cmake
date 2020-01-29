987bb0720e949c39d90ce9e91c95b1af949e51dc
Vladimir Migashko <migashko@gmail.com>
2020-01-29 13:27:43 +0300

    [github workflow] фикс. субмодулей после сборки

--- cmake/repos.cmake
+++ cmake/repos.cmake
@@ -1,9 +1,4 @@
-#Кастомный вариант для либы, в данном случае faslib
-set(faslib_REPO "git@github.lan:cpp/faslib.git")
-set(REPO_GROUP "cpp/")
-set(REPO_PREF "git@github.lan:")
-
-#set(faslib_REPO "https://github.com/migashko/faslib.git")
-#set(REPO_GROUP "mambaru/")
-#set(REPO_PREF "https://github.com/")
+set(faslib_REPO "https://github.com/migashko/faslib.git")
+set(REPO_GROUP "mambaru/")
+set(REPO_PREF "https://github.com/")
 
